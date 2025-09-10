919afae16274f600dca05ef177464cee129d895e
V3-Dump_Bot <girikhon2006@gmail.com>
2025-09-11 00:03:45 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -225,7 +225,7 @@ app.use(
 // app.use("/api/event", xssSanitizer(), event);
 app.use("/v1/api/user", userRoutes);
 app.use("/v1/api/profile", profile);
-app.use("v1//api/challenge", CTF);
+app.use("/v1/api/challenge", CTF);
 app.use("/v1/api/team", TeamRoutes);
 // CSRF route
 app.get(
