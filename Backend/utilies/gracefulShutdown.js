bc6cb3f6be18daa807a282b948e60e4f50c4b180
V3-Dump_Bot <girikhon2006@gmail.com>
2025-09-10 20:31:19 +0530

    Update gracefulShutdown.js

--- Backend/utilies/gracefulShutdown.js
+++ Backend/utilies/gracefulShutdown.js
@@ -1,26 +1,31 @@
-async function gracefulShutdown(loggerWorker) {
-  console.log('Shutting down: asking logger to flush...');
+async function gracefulShutdown(server, loggerWorker) {
+  console.log("Shutting down: asking logger to flush...");
   const FLUSH_TIMEOUT = 5000;
+
   const flushPromise = new Promise((resolve) => {
     const onMessage = (msg) => {
-      if (msg && msg.type === 'shutdownComplete') {
-        loggerWorker.removeListener('message', onMessage);
+      if (msg && msg.type === "shutdownComplete") {
+        loggerWorker.removeListener("message", onMessage);
         resolve(true);
       }
     };
-    loggerWorker.on('message', onMessage);
-    loggerWorker.postMessage({ type: 'shutdown' });
+
+    loggerWorker.on("message", onMessage);
+    loggerWorker.postMessage({ type: "shutdown" });
+
     setTimeout(() => {
-      loggerWorker.removeListener('message', onMessage);
+      loggerWorker.removeListener("message", onMessage);
       resolve(false);
     }, FLUSH_TIMEOUT);
   });
+
   const flushed = await flushPromise;
-  console.log('Logger flush acknowledged:', flushed);
+  console.log("Logger flush acknowledged:", flushed);
+
   server.close(() => {
-    console.log('HTTP server closed. Exiting process.');
+    console.log("HTTP server closed. Exiting process.");
     process.exit(flushed ? 0 : 1);
   });
 }
 
-module.exports=gracefulShutdown
+module.exports = gracefulShutdown;
